e670ece6989331db5579e8220363f3a7ce3a6080
gcheema77 <gcheema7@hotmail.com>
2023-07-24 13:25:28 -0600

    added inital structure

---
+++ love-calculator/index.js
@@ -0,0 +1,15 @@
+// Dice roll from 1 to 6
+// var n = Math.random();
+// n = n * 6;
+// n = Math.floor(n) + 1;
+// console.log(n);
+
+prompt("What is your name? " );
+prompt("What is their name? " );
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+(loveScore === 100) ? alert("Your love score is 100% " + "You love each other like kanye loves kanye"): alert("Your love score is " + loveScore + "%");
+
+alert("Your love score is " + loveScore + "%");
