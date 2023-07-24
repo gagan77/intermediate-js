63644399609391cae5acdbcaf808f9f75c665d6d
gcheema77 <gcheema7@hotmail.com>
2023-07-24 13:32:23 -0600

    if/else if

--- love-calculator/index.js
+++ love-calculator/index.js
@@ -10,6 +10,13 @@ prompt("What is their name? " );
 var loveScore = Math.random() * 100;
 loveScore = Math.floor(loveScore) + 1;
 
-(loveScore === 100) ? alert("Your love score is 100% " + "You love each other like kanye loves kanye"): alert("Your love score is " + loveScore + "%");
+// (loveScore === 100) ? alert("Your love score is 100% " + "You love each other like kanye loves kanye"): alert("Your love score is " + loveScore + "%");
+if(loveScore > 70){
+    alert("Your love score is 100% " + "You love each other like kanye loves kanye")
+} else if(loveScore > 30 && loveScore <= 70){
+    alert("Your love score is " + loveScore + "%");
+} else {
+    alert("Your love score is " + loveScore + "%" + " You go together like oil and water.");
+}
 
-alert("Your love score is " + loveScore + "%");
+
